31ff73935683939b46b9cec9825f6acd3bc7f794
seifer <t19854623@gmail.com>
2023-05-10 14:54:56 +0800

    代碼初始提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace csv_format_conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnReadCSV = new System.Windows.Forms.Button();
+            this.btnSaveUTF8 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusBar = new System.Windows.Forms.StatusBar();
+            this.Pastebutton = new System.Windows.Forms.Button();
+            this.UndoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadCSV
+            // 
+            this.btnReadCSV.Location = new System.Drawing.Point(41, 309);
+            this.btnReadCSV.Name = "btnReadCSV";
+            this.btnReadCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnReadCSV.TabIndex = 0;
+            this.btnReadCSV.Text = "Read CSV";
+            this.btnReadCSV.UseVisualStyleBackColor = true;
+            this.btnReadCSV.Click += new System.EventHandler(this.btnReadCSV_Click);
+            // 
+            // btnSaveUTF8
+            // 
+            this.btnSaveUTF8.Location = new System.Drawing.Point(150, 309);
+            this.btnSaveUTF8.Name = "btnSaveUTF8";
+            this.btnSaveUTF8.Size = new System.Drawing.Size(104, 23);
+            this.btnSaveUTF8.TabIndex = 1;
+            this.btnSaveUTF8.Text = "Save UTF8 CSV";
+            this.btnSaveUTF8.UseVisualStyleBackColor = true;
+            this.btnSaveUTF8.Click += new System.EventHandler(this.btnSaveUTF8_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(544, 283);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(0, 345);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(565, 25);
+            this.statusBar.SizingGrip = false;
+            this.statusBar.TabIndex = 56;
+            this.statusBar.Text = "Welcome!";
+            // 
+            // Pastebutton
+            // 
+            this.Pastebutton.Location = new System.Drawing.Point(333, 309);
+            this.Pastebutton.Name = "Pastebutton";
+            this.Pastebutton.Size = new System.Drawing.Size(75, 23);
+            this.Pastebutton.TabIndex = 57;
+            this.Pastebutton.Text = "貼上數據";
+            this.Pastebutton.UseVisualStyleBackColor = true;
+            this.Pastebutton.Click += new System.EventHandler(this.Pastebutton_Click);
+            // 
+            // UndoButton
+            // 
+            this.UndoButton.Location = new System.Drawing.Point(442, 309);
+            this.UndoButton.Name = "UndoButton";
+            this.UndoButton.Size = new System.Drawing.Size(75, 23);
+            this.UndoButton.TabIndex = 58;
+            this.UndoButton.Text = "撤銷上一步";
+            this.UndoButton.UseVisualStyleBackColor = true;
+            this.UndoButton.Click += new System.EventHandler(this.UndoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(565, 370);
+            this.Controls.Add(this.UndoButton);
+            this.Controls.Add(this.Pastebutton);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSaveUTF8);
+            this.Controls.Add(this.btnReadCSV);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSV Format Conversion - By:Seifer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadCSV;
+        private System.Windows.Forms.Button btnSaveUTF8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusBar statusBar;
+        private System.Windows.Forms.Button Pastebutton;
+        private System.Windows.Forms.Button UndoButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csv_format_conversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 是一個私有的 DataTable 變數，用來儲存上一次的 dataGridView1 資料。
+        // 這樣在進行撤銷操作時，可以將前一次的資料再次賦值給 dataGridView1，達到復原的目的。
+        private DataTable previousData;
+
+        private void btnReadCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // 檢測 CSV 文件的編碼格式
+                    Encoding encoding = GetEncoding(filePath);
+
+                    // 讀取 CSV 文件
+                    using (StreamReader reader = new StreamReader(filePath, encoding))
+                    {
+                        DataTable dataTable = new DataTable();
+                        bool isFirstRow = true;
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            string[] values = line.Split(',');
+                            if (isFirstRow)
+                            {
+                                foreach (string value in values)
+                                {
+                                    dataTable.Columns.Add(value.Trim());
+                                }
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                DataRow row = dataTable.NewRow();
+                                for (int i = 0; i < values.Length; i++)
+                                {
+                                    row[i] = values[i].Trim();
+                                }
+                                dataTable.Rows.Add(row);
+                            }
+                        }
+                        dataGridView1.DataSource = dataTable;
+
+                        // 顯示編碼格式
+                        statusBar.Text = "目前文件編碼格式為 : " + encoding.EncodingName;
+
+                        MessageBox.Show("文件已讀取，編碼格式為：" + encoding.EncodingName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    statusBar.Text = "讀取CSV文件時出現異常 : " + ex.Message;
+
+                    MessageBox.Show("讀取CSV文件時出現異常：" + ex.Message);
+                }
+            }
+        }
+
+        private Encoding GetEncoding(string filePath)
+        {
+            // 檢測文件的編碼格式
+            byte[] bytes = new byte[4];
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(bytes, 0, 4);
+            }
+
+            if (bytes[0] == 0xef && bytes[1] == 0xbb && bytes[2] == 0xbf)
+            {
+                return Encoding.UTF8;
+            }
+            else if (bytes[0] == 0xfe && bytes[1] == 0xff)
+            {
+                return Encoding.Unicode;
+            }
+            else if (bytes[0] == 0xff && bytes[1] == 0xfe)
+            {
+                return Encoding.BigEndianUnicode;
+            }
+            else
+            {
+                return Encoding.Default;
+            }
+        }
+
+
+        private void btnSaveUTF8_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    try
+                    {
+                        // 將數據寫入CSV文件
+                        using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
+                        {
+                            // 寫入列名
+                            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                            {
+                                writer.Write(dataGridView1.Columns[i].HeaderText);
+                                if (i < dataGridView1.ColumnCount - 1)
+                                {
+                                    writer.Write(",");
+                                }
+                            }
+                            writer.WriteLine();
+
+                            // 寫入數據
+                            for (int i = 0; i < dataGridView1.RowCount; i++)
+                            {
+                                if (dataGridView1.Rows[i].IsNewRow)
+                                {
+                                    continue;
+                                }
+
+                                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                                {
+                                    // 檢查單元格是否為空
+                                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                                    {
+                                        writer.Write(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                                    }
+                                    else
+                                    {
+                                        writer.Write("");
+                                    }
+                                    if (j < dataGridView1.ColumnCount - 1)
+                                    {
+                                        writer.Write(",");
+                                    }
+                                }
+                                writer.WriteLine();
+                            }
+
+                        }
+
+                        MessageBox.Show("文件已保存");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("保存CSV文件時出現異常：" + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("沒有可以保存的數據");
+            }
+        }
+
+        private void Pastebutton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource != null)
+            {
+                // 保存當前數據狀態
+                previousData = ((DataTable)dataGridView1.DataSource).Copy();
+
+                // 讀取剪貼板中的數據
+                string clipboardText = Clipboard.GetText();
+                string[] lines = clipboardText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                // 創建DataTable對象
+                DataTable dataTable = new DataTable();
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    dataTable.Columns.Add(dataGridView1.Columns[i].HeaderText);
+                }
+
+                // 添加數據
+                foreach (string line in lines)
+                {
+                    DataRow row = dataTable.NewRow();
+                    string[] values = line.Split('\t');
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        row[i] = values[i];
+                    }
+                    dataTable.Rows.Add(row);
+                }
+
+                // 將DataTable賦值給dataGridView1的DataSource
+                dataGridView1.DataSource = dataTable;
+
+                MessageBox.Show("數據貼上成功");
+            }
+            else
+            {
+                MessageBox.Show("沒有可以貼上的數據");
+            }
+        }
+
+        // 定義一個棧，用於記錄操作歷史
+        Stack<DataGridViewCell> cellStack = new Stack<DataGridViewCell>();
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewCell cell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                cellStack.Push(cell);
+            }
+        }
+
+        private void UndoButton_Click(object sender, EventArgs e)
+        {
+            if (previousData != null)
+            {
+                dataGridView1.DataSource = previousData;
+                MessageBox.Show("撤銷操作成功，當前數據已恢復到之前的狀態");
+            }
+            else
+            {
+                MessageBox.Show("沒有可以撤銷的數據");
+            }
+        }
+    }
+}
